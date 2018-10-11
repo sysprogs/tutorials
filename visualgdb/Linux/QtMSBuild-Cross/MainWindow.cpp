e8b353b9c4ad4ec90fcda9b8862d331aaf550d32
Sysprogs <sysprogs@sysprogs.com>
2018-10-11 11:38:48 -0700

    Added a sample Qt project

---
+++ visualgdb/Linux/QtMSBuild-Cross/MainWindow.cpp
@@ -0,0 +1,24 @@
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::ButtonClicked()
+{
+    QMessageBox msgBox;
+    msgBox.setText("Hello, World!");
+    msgBox.setWindowTitle("VisualGDB Qt Demo");
+    msgBox.exec();
+}
