d901e4fbc4e1f7f708487fe0b6d7b83f7cb6cae8
Sysprogs <sysprogs@sysprogs.com>
2018-11-18 22:33:58 -0800

    Added a basic real-time watch tutorial for the Keil/RTX environment

---
+++ visualgdb/keil/RTXRealTimeWatch/RTE/RTE_Components.h
@@ -0,0 +1,32 @@
+#pragma once
+#define CMSIS_device_header "stm32f4xx.h"
+
+        #define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX5             /* CMSIS-RTOS Keil RTX5 */
+      
+
+        #define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+      
+
+        #define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
+      
+
+        #define RTE_DEVICE_FRAMEWORK_CLASSIC
+      
+
+        #define RTE_DEVICE_HAL_COMMON
+      
+
+        #define RTE_DEVICE_HAL_CORTEX
+      
+
+        #define RTE_DEVICE_HAL_GPIO
+      
+
+        #define RTE_DEVICE_HAL_PWR
+      
+
+        #define RTE_DEVICE_HAL_RCC
+      
