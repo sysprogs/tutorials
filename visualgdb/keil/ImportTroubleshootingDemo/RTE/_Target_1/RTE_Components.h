6ecd7bddd523babcbc23788d5394a4e9c09c9b1c
Sysprogs <sysprogs@sysprogs.com>
2019-04-25 20:17:13 -0700

    Added a basic uVision project demonstrating optimization-dependent behavior

---
+++ visualgdb/keil/ImportTroubleshootingDemo/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,41 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ImportTroubleshootingDemo' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+#define RTE_DEVICE_FRAMEWORK_CLASSIC
+#define RTE_DEVICE_HAL_COMMON
+#define RTE_DEVICE_HAL_CORTEX
+#define RTE_DEVICE_HAL_DMA
+#define RTE_DEVICE_HAL_GPIO
+#define RTE_DEVICE_HAL_PWR
+#define RTE_DEVICE_HAL_RCC
+#define RTE_DEVICE_HAL_USART
+#define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
+#define RTE_Drivers_USART1              /* Driver USART1 */
+        #define RTE_Drivers_USART2              /* Driver USART2 */
+        #define RTE_Drivers_USART3              /* Driver USART3 */
+        #define RTE_Drivers_USART4              /* Driver USART4 */
+        #define RTE_Drivers_USART5              /* Driver USART5 */
+        #define RTE_Drivers_USART6              /* Driver USART6 */
+        #define RTE_Drivers_USART7              /* Driver USART7 */
+        #define RTE_Drivers_USART8              /* Driver USART8 */
+        #define RTE_Drivers_USART9              /* Driver USART9 */
+        #define RTE_Drivers_USART10             /* Driver USART10 */
+
+#endif /* RTE_COMPONENTS_H */
---
+++ visualgdb/keil/ImportTroubleshootingDemo/main.cpp
@@ -0,0 +1,57 @@
+/*----------------------------------------------------------------------------
+ * CMSIS-RTOS 'main' function template
+ *---------------------------------------------------------------------------*/
+ 
+extern "C" {
+ 
+#include "RTE_Components.h"
+#include  CMSIS_device_header
+#include "cmsis_os2.h"
+#include <Board_LED.h>
+
+}
+ 
+void Thread1Body(void *argument)
+{
+		int bigvar[4096];
+    for (;;)
+    {
+				bigvar[4095] = 0;
+        LED_On(0);
+        osDelay(100);
+        LED_Off(0);
+        osDelay(100);
+    }
+}
+
+void Thread2Body(void *argument)
+{
+    for (;;)
+    {
+        LED_On(1);
+        osDelay(200);
+        LED_Off(1);
+        osDelay(200);
+    }
+}
+ 
+int main(void) {
+ 
+    // System Initialization
+    SystemCoreClockUpdate();
+    LED_Initialize();
+    
+    const osThreadAttr_t thread1_attr = {
+         "Thread 1"
+    };
+ 
+    const osThreadAttr_t thread2_attr = {
+         "Thread 2"
+    };
+ 
+    osKernelInitialize();                  // Initialize CMSIS-RTOS
+    osThreadNew(Thread1Body, NULL, &thread1_attr);        // Create application main thread
+    osThreadNew(Thread2Body, NULL, &thread2_attr);        // Create application main thread
+    osKernelStart();                       // Start thread execution
+    for(;  ;) {}
+}
