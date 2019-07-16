02188379abfb61b4dc57296fb61ff6d21072cb82
Sysprogs <sysprogs@sysprogs.com>
2019-07-16 14:59:27 -0700

    Added basic M4 samples for STM32MP1

---
+++ visualgdb/ARM/stm32/stm32mp1-blink/stm32mp1-blink.cpp
@@ -0,0 +1,34 @@
+#include <STM32MP1xx_hal.h>
+#include <stm32_hal_legacy.h>
+
+#ifdef __cplusplus
+extern "C"
+#endif
+void SysTick_Handler(void)
+{
+	HAL_IncTick();
+	HAL_SYSTICK_IRQHandler();
+}
+
+int main(void)
+{
+	HAL_Init();
+
+	__GPIOH_CLK_ENABLE();
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	GPIO_InitStructure.Pin = GPIO_PIN_7;
+
+	GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStructure.Speed = GPIO_SPEED_FREQ_HIGH;
+	GPIO_InitStructure.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOH, &GPIO_InitStructure);
+
+	for (;;)
+	{
+		HAL_GPIO_WritePin(GPIOH, GPIO_PIN_7, GPIO_PIN_SET);
+		HAL_Delay(500);
+		HAL_GPIO_WritePin(GPIOH, GPIO_PIN_7, GPIO_PIN_RESET);
+		HAL_Delay(500);
+	}
+}
