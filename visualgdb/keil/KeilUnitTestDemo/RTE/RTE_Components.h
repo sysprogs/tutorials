7e8eee5589ed2bee8cd2e975a125acf33a844ea6
Sysprogs <sysprogs@sysprogs.com>
2020-05-14 21:36:51 -0700

    Added a tutorial on Keil-based unit test projects

---
+++ visualgdb/keil/KeilUnitTestDemo/RTE/RTE_Components.h
@@ -0,0 +1,23 @@
+#pragma once
+#define CMSIS_device_header "stm32f4xx.h"
+
+        #define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
+      
+
+        #define RTE_DEVICE_FRAMEWORK_CLASSIC
+      
+
+        #define RTE_DEVICE_HAL_COMMON
+      
+
+        #define RTE_DEVICE_HAL_CORTEX
+      
+
+        #define RTE_DEVICE_HAL_GPIO
+      
+
+        #define RTE_DEVICE_HAL_PWR
+      
+
+        #define RTE_DEVICE_HAL_RCC
+      
