7a21f74384f2f66f961af91f46a69813d43204fb
Sysprogs <sysprogs@sysprogs.com>
2020-05-26 18:12:55 -0700

    Added a basic NRFConnect tutorial

---
+++ visualgdb/iot/NRFConnect/ATCommandsDemo/src/main.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#include <zephyr.h>
+#include <stdio.h>
+#include <drivers/uart.h>
+#include <string.h>
+
+/**@brief Recoverable BSD library error. */
+void bsd_recoverable_error_handler(uint32_t err)
+{
+	printk("bsdlib recoverable error: %u\n", err);
+}
+
+void main(void)
+{
+	printk("The AT host sample started (with reset fix)\n");
+}
