153d04d47e749bf4cb62382f3c15821611f07058
Sysprogs <sysprogs@sysprogs.com>
2020-06-01 12:25:13 -0700

    Added a basic demo for the MAIXDuino board

---
+++ visualgdb/iot/maixduino/camera/MAIXDuinoCameraDemo/sketches/selfie.ino
@@ -0,0 +1,32 @@
+#include <Sipeed_OV2640.h>
+#include <Sipeed_ST7789.h>
+
+SPIClass spi_(SPI0); // MUST be SPI0 for Maix series on board LCD
+Sipeed_ST7789 lcd(320, 240, spi_);
+
+Sipeed_OV2640 camera(FRAMESIZE_QVGA, PIXFORMAT_RGB565);
+
+
+void setup()
+{
+    Serial.begin(115200);
+    lcd.begin(15000000, COLOR_RED);
+    if(!camera.begin())
+      Serial.printf("camera init fail\n");
+    else
+      Serial.printf("camera init success\n");
+    camera.run(true);
+}
+
+int g_FrameCounter;
+
+void loop()
+{
+    g_FrameCounter++;
+    uint8_t *img = camera.snapshot();
+    if (img == nullptr || img == 0)
+        Serial.printf("snap fail\n");
+    else
+        lcd.drawImage(0, 0, camera.width(), camera.height(), (uint16_t *)img);
+}
+
