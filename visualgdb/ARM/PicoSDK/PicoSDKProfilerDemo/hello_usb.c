bc653ba277b22f232a33252c38bdd2834efb1c13
Sysprogs <sysprogs@sysprogs.com>
2021-11-01 16:25:46 -0700

    Added a basic profiling tutorial for PicoSDK

---
+++ visualgdb/ARM/PicoSDK/PicoSDKProfilerDemo/hello_usb.c
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include <SysprogsProfiler.h>
+
+int main() {
+    stdio_init_all();
+    InitializeInstrumentingProfiler();
+
+    while (true) {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
+    return 0;
+}
