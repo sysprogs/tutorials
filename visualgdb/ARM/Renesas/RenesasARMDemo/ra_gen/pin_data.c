5151406eba549f5964deab33cee62f23f3d4edfa
Sysprogs <sysprogs@sysprogs.com>
2022-04-15 19:51:49 -0700

    Changed port direction for P000

--- visualgdb/ARM/Renesas/RenesasARMDemo/ra_gen/pin_data.c
+++ visualgdb/ARM/Renesas/RenesasARMDemo/ra_gen/pin_data.c
@@ -6,7 +6,7 @@
 #include "r_ioport_api.h"
 const ioport_pin_cfg_t g_bsp_pin_cfg_data[] =
 {
-	{ .pin = BSP_IO_PORT_00_PIN_00, .pin_cfg = ((uint32_t) IOPORT_CFG_ANALOG_ENABLE) },
+	{ .pin = BSP_IO_PORT_00_PIN_00, .pin_cfg = ((uint32_t) IOPORT_CFG_PORT_DIRECTION_OUTPUT | (uint32_t) IOPORT_CFG_PORT_OUTPUT_HIGH) },
 	{ .pin = BSP_IO_PORT_00_PIN_01, .pin_cfg = ((uint32_t) IOPORT_CFG_ANALOG_ENABLE) },
 	{ .pin = BSP_IO_PORT_00_PIN_02, .pin_cfg = ((uint32_t) IOPORT_CFG_ANALOG_ENABLE) },
 	{ .pin = BSP_IO_PORT_00_PIN_03, .pin_cfg = ((uint32_t) IOPORT_CFG_ANALOG_ENABLE) },
