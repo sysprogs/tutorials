58e12df3cd2efa0df1cce29f3abdf54bbe75264a
Sysprogs <sysprogs@sysprogs.com>
2022-04-15 19:53:25 -0700

    Referenced the ADC driver

--- visualgdb/ARM/Renesas/RenesasARMDemo/ra_gen/hal_data.c
+++ visualgdb/ARM/Renesas/RenesasARMDemo/ra_gen/hal_data.c
@@ -3,6 +3,64 @@
    Use VisualGDB Project Properties window to edit these settings instead. */
 
 #include "hal_data.h"
+/* Macros to tie dynamic ELC links to ADC_TRIGGER_SYNC_ELC option in adc_trigger_t. */
+#define ADC_TRIGGER_ADC0        ADC_TRIGGER_SYNC_ELC
+#define ADC_TRIGGER_ADC0_B      ADC_TRIGGER_SYNC_ELC
+#define ADC_TRIGGER_ADC1        ADC_TRIGGER_SYNC_ELC
+#define ADC_TRIGGER_ADC1_B      ADC_TRIGGER_SYNC_ELC
+
+
+adc_instance_ctrl_t g_adc0_ctrl;
+const adc_extended_cfg_t g_adc0_cfg_extend =
+{
+    .add_average_count   = ADC_ADD_OFF,
+    .clearing            = ADC_CLEAR_AFTER_READ_ON,
+    .trigger_group_b     = ADC_TRIGGER_SYNC_ELC,
+    .double_trigger_mode = ADC_DOUBLE_TRIGGER_DISABLED,
+    .adc_vref_control    = ADC_VREF_CONTROL_AVCC0_AVSS0,
+    .enable_adbuf        = 0,
+};
+const adc_cfg_t g_adc0_cfg =
+{
+    .unit                = 0,
+    .mode                = ADC_MODE_SINGLE_SCAN,
+    .resolution          = ADC_RESOLUTION_12_BIT,
+    .alignment           = (adc_alignment_t) ADC_ALIGNMENT_RIGHT,
+    .trigger             = ADC_TRIGGER_SOFTWARE,
+    .p_callback          = NULL,
+    .p_context           = NULL,
+    .p_extend            = &g_adc0_cfg_extend,
+#if defined(VECTOR_NUMBER_ADC0_SCAN_END)
+    .scan_end_irq        = VECTOR_NUMBER_ADC0_SCAN_END,
+#else
+    .scan_end_irq        = FSP_INVALID_VECTOR,
+#endif
+    .scan_end_ipl        = (BSP_IRQ_DISABLED),
+#if defined(VECTOR_NUMBER_ADC0_SCAN_END_B)
+    .scan_end_b_irq      = VECTOR_NUMBER_ADC0_SCAN_END_B,
+#else
+    .scan_end_b_irq      = FSP_INVALID_VECTOR,
+#endif
+    .scan_end_b_ipl      = (BSP_IRQ_DISABLED),
+};
+const adc_channel_cfg_t g_adc0_channel_cfg =
+{
+    .scan_mask           =  0,
+    .scan_mask_group_b   =  0,
+    .priority_group_a    = ADC_GROUP_A_PRIORITY_OFF,
+    .add_mask            =  0,
+    .sample_hold_mask    =  0,
+    .sample_hold_states  = 24,
+};
+/* Instance structure to use this module. */
+const adc_instance_t g_adc0 =
+{
+    .p_ctrl    = &g_adc0_ctrl,
+    .p_cfg     = &g_adc0_cfg,
+    .p_channel_cfg = &g_adc0_channel_cfg,
+    .p_api     = &g_adc_on_adc
+};
+
 void g_hal_init(void)
 {
     g_common_init ();
--- visualgdb/ARM/Renesas/RenesasARMDemo/ra_gen/hal_data.h
+++ visualgdb/ARM/Renesas/RenesasARMDemo/ra_gen/hal_data.h
@@ -7,7 +7,26 @@
 #include <stdint.h>
 #include "bsp_api.h"
 #include "common_data.h"
+#include "r_adc.h"
+#include "r_adc_api.h"
+
 FSP_HEADER
+/** ADC on ADC Instance. */
+extern const adc_instance_t g_adc0;
+
+/** Access the ADC instance using these structures when calling API functions directly (::p_api is not used). */
+extern adc_instance_ctrl_t g_adc0_ctrl;
+extern const adc_cfg_t g_adc0_cfg;
+extern const adc_channel_cfg_t g_adc0_channel_cfg;
+
+#ifndef NULL
+void NULL(adc_callback_args_t * p_args);
+#endif
+
+#ifndef NULL
+#define ADC_DMAC_CHANNELS_PER_BLOCK_NULL  0
+#endif
+
 void hal_entry(void);
 void g_hal_init(void);
 FSP_FOOTER
